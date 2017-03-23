2cb9900a36ead0204ad12e06053290656bb187db
= <=>
2017-03-23 18:10:17 +0800

    some slight work

---
+++ app/routes.js
@@ -0,0 +1,20 @@
+// //import modules
+// var express = require('express');
+// var path = require('path');
+
+// //create objects
+// var router = express.Router();
+
+// //delivering views
+// //res.sendFile()
+// //path.join() for generating file path
+// //__dirname for the directory name
+
+// //routing
+// //route for homepage
+// //router.get(path, handler) for GET request
+
+// //router.post(path, handler) for POST request
+
+// //module export
+// module.exports = router;
