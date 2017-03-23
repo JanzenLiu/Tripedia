83447df3728eab78649b935405077b3d379cfa00
= <chengtsung_liu@yahoo.com>
2017-03-24 02:07:32 +0800

    Attempt to achieve storing data to local database instead of to the memory only when the MongoDB is running

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
 var mongoose = require('mongoose');
-mongoose.connect('localhost','tests');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
+var store = new MongoStore({
+	uri: 'mongodb://localhost:27017:/connect-mongo-test',
+	collection: 'test005'
+})
+
+//////////////////////////// NativeConnection ///////////////////////
+// mongoose.connect('localhost','tests', function(err){
+// 	if(err)
+// 		console.log(err);
+// 	// else
+// 	// 	console.log(mongoose.connection);
+// });
+
+/////////////////////////// NativeConnection //////////////////////////
+var db = mongoose.createConnection('localhost', 'test');
+
+db.on('error', function(err){
+	console.log(err)
+});
+
+// db.once('open', function(){
+// 	console.log(db); 
+// })
+
+// ==================================================================
 var testSchema = mongoose.Schema({name : String});
 var testModel = mongoose.model('test', testSchema);
 
@@ -8,6 +34,15 @@ var test = new testModel({name : 'test'});
 test.save(function(err){
 	if(err)
 		console.log(err);
-	else
-		console.log('test has been saved');
+	// else
+	// 	console.log(test.name + " " + test._id);
 });
+// testModel.save();
+
+console.log(mongoose.connection.collections);
+db.close(function(err){
+	if(err)
+		console.log(err);
+})
+
+// console.log(mongoose.connection.collections);
