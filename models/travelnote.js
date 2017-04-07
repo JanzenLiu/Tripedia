a1f033051c16d6a93de0d2a69d3f2f9b9c78e5d1
Max Sum <maxsumprc@gmail.com>
2017-04-07 18:31:13 +0800

    Initial design of travelnote

--- models/travelnote.js
+++ models/travelnote.js
@@ -1,19 +1,20 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // Schema for TravelNote ========================
-var noteSchema = mongoose.Schema({
+var noteSchema = Schema({
 	title 				: String,
-	author				: Schema.Types.ObjectId,
+	author				: {type: Schema.Types.ObjectId, ref: 'User'},
 	content				: String,
 	word_count			: Number,
 	create_time			: Timestamp,
 	moddify_time 		: Timestamp,
 	pv					: Number, // increment by time every one visited
 	like_count			: Number,
-	likes 				: [{uid: ObjectId}],
+	likes 				: [{type: Schema.Types.ObjectId, ref: 'User'}],
 	comment_count		: Number,
 	comments			: [{
-		uid: ObjectId,
+		author: {type: Schema.Types.ObjectId, ref: 'User'},
 		message: String,
 		create_time: Timestamp
 	}]
