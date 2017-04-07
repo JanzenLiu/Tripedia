34c618e503bc3322e79872b15782fd2b8d2e2567
= <chengtsung_liu@yahoo.com>
2017-04-08 02:18:28 +0800

    install multer module

---
+++ controllers/search.js
@@ -0,0 +1,24 @@
+var User = require('../models/user');
+var Note = require('../models/travelnote');
+var City = require('../models/city');
+var Attraction = require('../models/attraction');
+
+var searchByUser = function(key){};
+
+module.exports = function(req, res){
+	var byUser = [],
+		byNote = [],
+		byCity = [],
+		byAttraction = [];
+
+	// manipulation...
+
+	// will it work? JS is an asynchronized language...
+	var results = {
+		users: byUser,
+		notes: byNote,
+		cities: byCity,
+		attractions: byAttraction
+	};
+	return results;
+}
