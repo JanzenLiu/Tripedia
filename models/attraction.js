b86ba1aa8a708768fb48aee1b744fe564b249b6f
= <chengtsung_liu@yahoo.com>
2017-04-11 02:02:18 +0800

    modify model for pois and script to merge data

--- models/attraction.js
+++ models/attraction.js
@@ -3,24 +3,25 @@ var Connection = require('./db');
 
 
 // Schema for Attraction ========================
-var poiSchema = new Schema({
+var poiSchema = new mongoose.Schema({
     name: String,
     types: [String], 
     cityName: String,
     cityPath: String,
     introduction: [String],
+    imageUrl: String,
     contact: [String],
     // phone: String,
     // website: String,
     location: String,
     position: {
-        longtitude: Double, // or String, or add validation?
-        langtitude: Double
-    }
+        longtitude: String, // or String, or add validation?
+        langtitude: String
+    },
     ticket: String,
     openTime: String,
     comments: [{
-        uid: ObjectId,
+        uid: mongoose.Schema.Types.ObjectId,
         name: String,
         body: String
     }]
@@ -28,6 +29,9 @@ var poiSchema = new Schema({
     /////////////// to be supplement //////////////////
 });
 
+poiSchema.statics.findById = function(id, cb){
+    return this.findOne({_id: mongoose.Types.ObjectId(id)}, cb);
+};
 
 module.exports = mongoose.model('poi', poiSchema);
 
--- routes/poi.js
+++ routes/poi.js
@@ -1,4 +1,28 @@
 var express = require('express');
+var Poi = require('../models/attraction');
 var router = express.Router();
+
+router.get('/:poiId', function(req, res){
+
+	Poi.findById(req.params.poiId, function(err, poi){
+		if(err || !poi){
+			req.flash('error', 'Attraction not found!');
+			return res.status(500).json({
+				error: 'Attraction not found',
+				success: false
+			});
+		}
+
+		console.log(poi);
+
+		res.render('city',{
+			title: poi.name,
+			user: req.session.user,
+			city: poi,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		});
+	});
+});
 
 module.exports = router;
